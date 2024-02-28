2b7c7c3a77c2ba706cbb33e90ee2fc923336f086
Rylan Conley <rconley@udel.edu>
2024-02-28 18:30:10 -0500

    I think start attempt works

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,31 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [progress, setProgress] = useState<boolean>(false);
+    const [attempts, setAttempts] = useState<number>(4);
+
+    function increaseAttempt(): void {
+        setAttempts(attempts + 1);
+    }
+    function startQuiz(): void {
+        setProgress(true);
+        setAttempts(attempts - 1);
+    }
+    function stopQuiz(): void {
+        setProgress(false);
+    }
+
+    return (
+        <div>
+            <Button onClick={startQuiz} disabled={progress || attempts === 0}>
+                Start Quiz
+            </Button>
+            <Button onClick={stopQuiz} disabled={!progress}>
+                Stop Quiz
+            </Button>
+            <Button onClick={increaseAttempt} disabled={progress}>
+                Mulligan
+            </Button>
+        </div>
+    );
 }
