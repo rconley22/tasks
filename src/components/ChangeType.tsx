2d8df84f1864e7095fb656f23b4ba86027a4dcaa
Rylan Conley <rconley@udel.edu>
2024-02-28 17:12:52 -0500

    Created changeType

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,27 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const types: QuestionType[] = [
+        "short_answer_question",
+        "multiple_choice_question"
+    ];
+    const [type, setType] = useState<QuestionType>(types[0]);
+
+    function setTyp(): void {
+        const newType =
+            type === "multiple_choice_question"
+                ? "short_answer_question"
+                : "multiple_choice_question";
+        setType(newType);
+    }
+
+    return (
+        <div>
+            <Button onClick={setTyp}>Change Type</Button>
+            {(type === "multiple_choice_question" && (
+                <div>Multiple choice</div>
+            )) ||
+                (type === "short_answer_question" && <div>Short answer</div>)}
+        </div>
+    );
 }
