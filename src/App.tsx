5d0d1b1942b0a2b58a503f2fd1f6c7959c338f36
Rylan Conley <rconley@udel.edu>
2024-03-01 18:48:42 -0500

    Passing DoubleHalf

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App(): JSX.Element {
                 World.
             </header>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            {<DoubleHalf></DoubleHalf>}
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
