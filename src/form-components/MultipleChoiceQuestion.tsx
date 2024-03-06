bcaf51936c04f23759dd18ea736e7b79937c005c
Rylan Conley <rconley@udel.edu>
2024-03-06 11:39:12 -0500

    multiple choice works

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,25 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [currentChoice, setCurrentChoice] = useState<string>(options[0]);
+
+    function updateChoice(event: React.ChangeEvent<HTMLSelectElement>) {
+        setCurrentChoice(event.target.value);
+    }
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="multipleChoice">
+                <Form.Label>Please select an answer</Form.Label>
+                <Form.Select value={currentChoice} onChange={updateChoice}>
+                    {options.map((options: string) => (
+                        <option key={options} value={options}>
+                            {options}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {currentChoice === expectedAnswer ? "✔️" : "❌"}
         </div>
     );
 }
